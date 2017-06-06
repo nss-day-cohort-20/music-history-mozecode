eb79159666942daf16b0b63c34f01a56493396db
Greta Moseley <greta.moseley@gmail.com>
2017-06-05 20:40:27 -0500

    update js to replace unwanted characters, set up preliminary functions to print to div

---
+++ songs.js
@@ -0,0 +1,63 @@
+var songs = [];
+
+songs[songs.length] = "Legs > by Z*ZTop on the album Eliminator";
+songs[songs.length] = "The Logical Song > by Supertr@amp on the album Breakfast in America";
+songs[songs.length] = "Another Brick in the Wall > by Pink Floyd on the album The Wall";
+songs[songs.length] = "Welco(me to the Jungle > by Guns & Roses on the album Appetite for Destruction";
+songs[songs.length] = "Ironi!c > by Alanis Moris*ette on the album Jagged Little Pill";
+
+var newSong = songs.push("Eyes Of The World - by The Grateful Dead on the album One From The Vault");
+
+var newSong2 = songs.unshift("Arsonist's Lullabye - by Hozier on the album From Eden");
+
+for(var j=0; j<songs.length; j++){
+
+    songs[j] = songs[j].replace (/@/g, '');   
+    songs[j] = songs[j].replace (/\(/g, '');
+    songs[j] = songs[j].replace (/\*/g, '');
+    //have to use an escape character to do ( and *
+    songs[j] = songs[j].replace (/!/g, ''); 
+    songs[j] = songs[j].replace (/>/g, "-");
+}
+
+//make it one big string, find index of items I need
+ //need song title, artist, album separated from
+ //string 
+ //and put into arrays or object to be accessed 
+ //below?
+
+ //what about genre?
+
+
+function addSongToDiv(message) {
+    var wrapperDiv = document.getElementById("wrapper");
+    wrapperDiv.innerHTML = message;
+}
+
+var html = "";
+
+function iterateThroughArray(arr) {
+    
+    for (var i = 0; i < songs.length; i++) {
+        
+            message =`<div class="songinfo">
+				<h2>${  }</h2>
+				<ul>
+					<li>${  }</li>
+					<li>${  }</li>
+					<li>Rock</li>
+				</ul>
+			</div>`
+
+        html += message;
+        
+        
+    }
+ addSongToDiv(html);
+}
+//iterate through and remove unwanted characters and words
+//replace > with -
+//add a song to the beginning and end of the array
+//add each string to the DOM in the right place
+
+iterateThroughArray(songs);
